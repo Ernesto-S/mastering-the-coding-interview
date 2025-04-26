b6796dd85022c56f44c14a09ea714e3c74e0a5a9
Ernesto-S <ernesto.salcedo.engr@gmail.com>
2025-04-26 16:26:05 -0700

    created a hashTable in python.

---
+++ 4_Data_Structures/Hash_Tables/DS_Hash_Tables.md
@@ -0,0 +1,5 @@
+# Hash_Tables_Notes
+
+In python Hash Tables are called dictionaries. These are data sets where you have a key and a value. The key is used to find the values in memory. 
+
+- A downside of Hash tables is something called "Collision". Collision is when data is saved in the same memory location. This results in speed reduction. 
---
+++ 4_Data_Structures/Hash_Tables/HashTable.py
@@ -0,0 +1,49 @@
+
+class HashTable:
+    def __init__ (self, size):
+        self.size = size
+        self.table = [None] * size
+
+    def hash_function(self, key):
+
+        if isinstance(key, str):
+            return sum(ord(char) for char in key) % self.size
+        elif isinstance(key, int):
+            return key % self.size
+        else:
+            raise TypeError("Key must be an integer or string")
+        
+    
+    def insert(self, key, value):
+        index = self.hash_function(key)
+        if self.table[index] is None:
+            self.table[index] = [(key, value)]
+        else:
+            for i, (k, v) in enumerate(self.table[index]):
+                if k == key:
+                    self.table[index][i] = (key, value)
+                    return
+            self.table[index].append((key, value))
+    
+    def search(self, key):
+        index = self.hash_function(key)
+        if self.table[index] is not None:
+            for k, v in self.table[index]:
+                if k == key:
+                    return v
+        return None
+
+    def delete(self, key):
+        index = self.hash_function(key)
+        if self.table[index] is not None:
+            for i, (k, v) in enumerate(self.table[index]):
+                if k == key:
+                    del self.table[index][i]
+                    return True
+        return False
+    
+myHashTable = HashTable(1)
+myHashTable.insert('grapes', 10000)
+myHashTable.insert('apples', 23)
+print(myHashTable.table) 
+print(myHashTable.search('apples'))  # Output: one

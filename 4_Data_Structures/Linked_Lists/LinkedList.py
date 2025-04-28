308b3ac87b8ee5e90fce74a6be0ce83d9cc87390
Ernesto-S <ernesto.salcedo.engr@gmail.com>
2025-04-27 22:14:50 -0700

    adding insert and delete to linked list

--- 4_Data_Structures/Linked_Lists/LinkedList.py
+++ 4_Data_Structures/Linked_Lists/LinkedList.py
@@ -1,6 +1,13 @@
 # Create a linked list
 #  10-->5-->16
 
+class Node:
+    def __init__(self, value):
+        self.node = {
+            "value": value,
+            "next": None
+        }
+
 class myLinkedList:
     def __init__(self, value):
         self.head = {
@@ -26,25 +33,70 @@ class myLinkedList:
         self.head = newHead
         self.length += 1
 
+    def printList(self):
+        currentNode = self.head
+        while currentNode is not None:
+            print(currentNode["value"])
+            currentNode = currentNode["next"]
+
+    def insert(self, index, value):
+        # check parammeters
+        if index >= self.length:
+            return self.append(value)
+        
+        newNode = {
+            "value": value,
+            "next": None
+        }
+
+        leader = self.traverseToIndex(index-1)
+        holdingPointer = leader["next"]
+        leader["next"] = newNode
+        newNode["next"] = holdingPointer
+        self.length += 1
+
+
+    def traverseToIndex(self, index):
+        counter = 0
+        currentNode = self.head
+        while counter != index:
+            currentNode = currentNode["next"]
+            counter += 1
+        return currentNode
+    
+    def delete(self, index):
+        if index >= self.length:
+            return False
+        
+        leader = self.traverseToIndex(index-1)
+        unwantedNode = leader["next"]
+        leader["next"] = unwantedNode["next"]
+        self.length -= 1
+        return True
+        
 
 LL = myLinkedList(10)
-print("\nInitial Head", LL.head)
-print("Initial Tail", LL.tail)
-print("Initial Length", LL.length)
+print("Initial")
+LL.printList()
 
 LL.append(5)
-print("\nAppended Head", LL.head)
-print("Appended Tail", LL.tail)
-print("Appended length", LL.length)
+print("\nAppended 5")
+LL.printList()
 
 LL.append(16)
-print("\nAppended Head", LL.head)
-print("Appended Tail", LL.tail)
-print("Appended length", LL.length)
+print("\nAppended 16")
+LL.printList()
 
 LL.prepend(1)
-print("Prepended Head", LL.head)
-print("Prepended Tail", LL.tail)
-print("Prepended length", LL.length, "\n")
+print("\nPrepended 1")
+LL.printList()
+
+LL.insert(2, 99)
+print("\nInserted 99 at index 2")
+LL.printList()
+
+LL.delete(2)
+print("\nDeleted index 2")
+LL.printList()
 
 

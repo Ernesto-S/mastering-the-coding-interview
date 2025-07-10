5d40be03fab9aa957c0a80052f6eded806646783
Ernesto-S <ernesto.salcedo.engr@gmail.com>
2025-07-09 19:22:13 -0700

    recursion function of the fibonacci sequence.

--- 5_Algorithms/Recursion/Algo_Recursion.md
+++ 5_Algorithms/Recursion/Algo_Recursion.md
@@ -6,6 +6,8 @@ Is a common concept used to refer a function within itself. These are really goo
 
 When working with recursion it can potentially cause a stack overflow issue. This can be dangerous since infinite loops can occur. The downside is that it also can take up more memory. 
 
+the big O of recursion could be exponential issue. 2^n
+
 # Mitigation
 
 Creating a base case. 
--- 5_Algorithms/Recursion/recursion.py
+++ 5_Algorithms/Recursion/recursion.py
@@ -7,7 +7,7 @@ def inception():
     counter += 1
     return inception()
 
-print(inception())
+# print(inception())
 
 # Write two functions that finds the factorial of any number.One should use recursive, the other should just use a for loop.
 N = 1
@@ -25,7 +25,30 @@ def find_factorial_iterative(n):
         factorial = factorial*i
     return factorial
 
-print("Interactive")
-print(find_factorial_iterative(5))
-print("Recursive")
-print(find_factorial_recursive(6))
+# print("Interactive")
+# print(find_factorial_iterative(5))
+# print("Recursive")
+# print(find_factorial_recursive(6))
+
+# Given a number N return the index value of the Fibonacci sequence, where the sequence is:
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
+# The pattern of the sequence is that each value is the sum of the 2 previous values, that means that for N=5 -> 2+3
+
+def iterative_fibonacci(n):
+    fib = [0, 1]
+    
+    for i in range(2, n+1):
+        fib.append(fib[i-2]+fib[i-1])
+    return fib[n]    
+
+print(iterative_fibonacci(40))
+
+fibonacci = 0
+def recursive_fibonacci(n):
+    global fibonacci
+    if n < 2:
+        return n
+    
+    return recursive_fibonacci(n-1) + recursive_fibonacci(n-2)
+
+print(recursive_fibonacci(40))
